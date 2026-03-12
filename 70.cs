924f9b420cb8c0d02148b7df55d3ff9bd7d66d5a
Phil Rebeiro <128755119+Cheese5teak@users.noreply.github.com>
2026-03-11 20:00:13 -0400

    Update 70.cs

--- 70.cs
+++ 70.cs
@@ -12,4 +12,23 @@ public class Solution {
 
         return ways[n];
     }
+
+
+    /*
+    Alternative (less memory)
+    public int ClimbStairs(int n) 
+    {
+        int one=1; //one step
+        int two=1; //two step
+
+        for(int i=0; i<n-1;i++) //note we only loop n-1 times
+        {
+            int temp=one; //store 1
+            one+=two; //1=1+2
+            two=temp; //2=old 1
+        }
+
+        return one; //return 1
+    }
+    */
 }
